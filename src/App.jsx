22a2842305669b28caf331a5fb067a4f06983780
dushaboev <dushaboyevdoston0@email.com>
2023-11-14 23:04:12 +0500

    firs commit

---
+++ src/App.jsx
@@ -0,0 +1,109 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  // const [button, setbutton] = useState(1);
+  // const [string, setstring] = useState('ali');
+  // const [string, setstring] = useState('Tugmani bosing');
+  // const [count, setCount] = useState(false);
+  // const [count1, setCount1] = useState(false);
+  // const [count2, setCount2] = useState(false);
+  // const [count3, setCount3] = useState(false);
+  // const [count4, setCount4] = useState(false);
+  // const [count5, setCount5] = useState(false);
+  // const [count6, setCount6] = useState(false);
+  // const [count7, setCount7] = useState(false);
+  // const [count8, setCount8] = useState(false);
+  // const [count9, setCount9] = useState(false);
+  // const [count10, setCount10] = useState(false);
+  // const [use, setuse] = useState({ name: 'ali'});
+  // const [user, setUser] = useState({ name: 'Ali' });
+
+  // const updateUserName = () => {
+  //   setUser({ ...user, name: 'Vali' });
+  // };
+
+ 
+
+  // const createButton = (text, clickHandler) => (
+  //   <button key={text} onClick={clickHandler}>
+  //     {text}
+  //   </button>
+  // );
+
+  // const Button = (value) => {
+  //   setbutton(value);
+  // };
+  
+  // const Button = () => {
+  //   setbutton('salom');
+  // };
+
+  return (
+    <>
+            {/* <button onClick={Button}>{button}</button> */}
+
+     {/* <p>Siz {button} marta bosdingiz</p>
+      <button onClick={() => setbutton(button + 1)}>
+        Bosish
+      </button> */}
+
+       {/* <h2>{button}</h2>
+    <button onClick={() => setbutton((num) => num + 1)}>+</button>
+    <button onClick={() => setbutton((num) => num - 1)}>-</button> */}
+
+      {/* <h2>Current State: {button}</h2>
+      <div>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((value) => (
+          <button key={value} onClick={() => Button(value)}>
+            Set State {value}
+          </button>
+        ))}
+      </div> */}
+
+
+
+
+
+      {/* <h2>{string}</h2>
+      <input type="text" name="" id="" value={string} onChange={(e) => setstring(e.target.value)} /> */}
+      
+      {/* {createButton(string, () => setstring('Salom'))} */}
+      
+      {/* <h2>{count ? 'hello': 'bye' }</h2>
+    <button onClick={() => setCount(true)}>no</button>
+    <button onClick={() => setCount(false)}>yes</button> */}
+
+{/* <p>{`Count 1: ${count1.toString()}`}</p>
+      <p>{`Count 2: ${count2.toString()}`}</p>
+      <p>{`Count 3: ${count3.toString()}`}</p>
+      <p>{`Count 4: ${count4.toString()}`}</p>
+      <p>{`Count 5: ${count5.toString()}`}</p>
+      <p>{`Count 6: ${count6.toString()}`}</p>
+      <p>{`Count 7: ${count7.toString()}`}</p>
+      <p>{`Count 8: ${count8.toString()}`}</p>
+      <p>{`Count 9: ${count9.toString()}`}</p>
+      <p>{`Count 10: ${count10.toString()}`}</p>
+
+      <button onClick={() => setCount1(!count1)}>Toggle 1</button>
+      <button onClick={() => setCount2(!count2)}>Toggle 2</button>
+      <button onClick={() => setCount3(!count3)}>Toggle 3</button>
+      <button onClick={() => setCount4(!count4)}>Toggle 4</button>
+      <button onClick={() => setCount5(!count5)}>Toggle 5</button>
+      <button onClick={() => setCount6(!count6)}>Toggle 6</button>
+      <button onClick={() => setCount7(!count7)}>Toggle 7</button>
+      <button onClick={() => setCount8(!count8)}>Toggle 8</button>
+      <button onClick={() => setCount9(!count9)}>Toggle 9</button>
+      <button onClick={() => setCount10(!count10)}>Toggle 10</button>  */}
+       
+       {/* <h2>{ use.name}</h2> */}
+
+       {/* <p>{`User Name: ${user.name}`}</p>
+      <button onClick={updateUserName}>Update Name</button> */}
+      
+
+    </>
+  );
+}
+
+export default App;
